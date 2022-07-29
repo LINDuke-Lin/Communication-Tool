d574f344f60973f16dad53179e12519309ea0ba7
duke <hibary.taiwan@hotmail.com>
2022-07-29 22:02:21 +0800

    first commit

---
+++ Src/Communication.Mqtt/Services/ConnectService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communication.Mqtt.Services
+{
+    internal static class ConnectService
+    {
+        private static void Subscribe()
+        {
+
+        }
+    }
+}
