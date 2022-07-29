06027d1750710a00975fa86ee1e37380e035a936
Chicken <hibary.taiwan@gmial.com>
2022-07-29 22:43:05 +0800

    feat: subscribe

---
+++ Src/Communication.Mqtt/Models/ConnectModel.cs
@@ -0,0 +1,85 @@
+﻿namespace Communication.Mqtt.Models
+{
+    public class ConnectModel
+    {
+        private string? _ServerIp;
+        private string? _MqttUser;
+        private string? _MqttPassword;
+        private string? _ClientId;
+
+        /// <summary>
+        /// Mqtt ip
+        /// </summary>
+        public string ServerIp
+        {
+            get => _ServerIp;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+
+                _ServerIp = value;
+            }
+        }
+
+        /// <summary>
+        /// Mqtt port
+        /// </summary>
+        public int ServerPort { get; set; }
+
+        /// <summary>
+        /// Mqtt User
+        /// </summary>
+        public string MqttUser
+        {
+            get => _MqttUser;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+
+                _MqttUser = value;
+            }
+        }
+
+        /// <summary>
+        /// Mqtt Password
+        /// </summary>
+        public string MqttPassword
+        {
+            get => _MqttPassword;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+
+                _MqttPassword = value;
+            }
+        }
+
+        /// <summary>
+        /// Mqtt PRE_TRANSFER
+        /// </summary>
+        public string? PRE_TRANSFER { get; set; }
+
+        /// <summary>
+        /// Mqtt 主題
+        /// </summary>
+        public string? Topic { get; set; }
+
+        /// <summary>
+        /// Mqtt ClientId
+        /// </summary>
+        public string ClientId
+        {
+            get => _ClientId;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+
+                _ClientId = value;
+            }
+        }
+    }
+}
--- Src/Communication.Mqtt/Services/ConnectService.cs
+++ Src/Communication.Mqtt/Services/ConnectService.cs
@@ -1,16 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Communication.Mqtt.Models;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace Communication.Mqtt.Services
 {
     internal static class ConnectService
     {
-        private static void Subscribe()
+        private static MqttClient? _mqttClient;
+        private static Thread? t_mqtt;
+        private static ConnectModel connectModel =new();
+
+        private static void _TryContinueConnect()
+        {
+            if (_mqttClient.IsConnected) return;
+
+            Thread retryThread = new Thread(new ThreadStart(delegate
+            {
+                Subscribe();
+            }));
+
+            retryThread.Start();
+        }
+        private static void SetClient()
+        {
+            _mqttClient = new MqttClient(connectModel.ServerIp, connectModel.ServerPort, false, null, null, MqttSslProtocols.TLSv1_2);
+            if (_mqttClient != null)
+            {
+                _mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishTransferReceived;
+                _mqttClient.ConnectionClosed += (sender, e) =>
+                {
+                    _TryContinueConnect();
+                };
+            }
+        }
+        private static void client_MqttMsgPublishTransferReceived(object sender, MqttMsgPublishEventArgs e)
         {
 
+        }
+        private static void Subscribe()
+        {
+            while (_mqttClient == null || _mqttClient.IsConnected == false)
+            {
+                if (_mqttClient == null)
+                    SetClient();
+                else
+                    _mqttClient.Connect(connectModel.ClientId, connectModel.MqttUser, connectModel.MqttPassword, false, 20);
+
+
+                if (_mqttClient.IsConnected)
+                {
+                    _mqttClient.Subscribe(new String[] { connectModel.PRE_TRANSFER + connectModel.Topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                    break;
+                }
+            }
+        }
+
+
+        public static void ConnectMqtt(ConnectModel _connectModel)
+        {
+            if(connectModel == null)
+                throw new ArgumentNullException(nameof(connectModel));
+
+            connectModel = connectModel;
+            t_mqtt = new Thread(Subscribe);
+            t_mqtt.Start();
         }
     }
 }
