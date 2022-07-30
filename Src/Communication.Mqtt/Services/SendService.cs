e82226287e9ad1a1b0e901334394f7ad848d6102
Chicken <hibary.taiwan@gmial.com>
2022-07-30 18:49:49 +0800

    feat: send message

---
+++ Src/Communication.Mqtt/Services/SendService.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Communication.Mqtt.Services
+{
+    public class SendService
+    {
+        public void Send(string topic, string msg)
+        {
+            if (ConnectService._mqttClient == null || ConnectService._mqttClient.IsConnected == false)
+                throw new Exception("尚未連線");
+
+            ConnectService._mqttClient.Publish(topic, Encoding.UTF8.GetBytes(msg), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+        }
+    }
+}
